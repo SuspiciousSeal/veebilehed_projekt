c1d2f4bcff5a7cb0e1e633859467df75b3a03a7f
LAPTOP-N55O18KP\Robert <robert.allik@protonmail.com>
2023-10-25 19:52:25 +0300

    Looks nicer

--- nav.js
+++ nav.js
@@ -1,4 +1,5 @@
 document.write(`
+<div id="spacer"></div>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
